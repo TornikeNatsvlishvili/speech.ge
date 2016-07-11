f223f741771dab92d9e1b59dba6106240aae0351
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-07-10 23:41:37 -0700

    add command line arguments to specify protocol

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+import sys
 from app.app import app
-context = ('domain.crt', 'domain.key')
-app.run(host='0.0.0.0', debug=True, port=8080, ssl_context=context)
+
+if len(sys.argv) == 1:
+    print("Requires an argument: http or https")
+else:
+    if sys.argv[1] == 'http':
+        app.run(host='127.0.0.1', port=8080)
+    elif sys.argv[1] == 'https':
+        context = ('domain.crt', 'domain.key')
+        app.run(host='0.0.0.0', debug=True, port=8080, ssl_context=context)
